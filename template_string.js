67856c4faf8dbff36f13fa5c021a7a788b594b40
asifemran890 <freelancerasif890@gmail.com>
2025-09-02 09:27:56 +0600

    template string

--- template_string.js
+++ template_string.js
@@ -1,14 +1,18 @@
-// var name = "Ahashan Habib Utsho"
+var name = "Ahashan Habib Utsho";
 
 // Ternary operator -> condition ? true : false
 var age = 25;
 var name = `Ahashan Habib
  Utsho. My age is ${age}. I am ${age < 18 ? "not" : ""} adult.`;
-
+// console.log(name);
+var age = 2;
+var name = ` Asif emrna ....my age is ${age}. i am ${
+  age < 18 ? "not" : ""
+} adult.`;
 // console.log(name);
 
-// padStart , padEnd, repeat
+// padStart, padEnd, repeat;
 var test = "demo ";
-console.log(test.padStart(10, "-"));
-console.log(test.padEnd(10, "*"));
-console.log(test.repeat(10));
+console.log(test.padStart(15, "*"));
+console.log(test.padEnd(15, "*"));
+console.log(test.repeat(15));
